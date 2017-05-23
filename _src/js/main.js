21f670a0ac0c6ea6008dc863fcdc9ddea5ef2722
Luca De Blasio <deblaisioluca@gmail.com>
2017-05-23 15:20:30 +0200

    update

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,18 +1,5 @@
-
-// import './vendor/TweenMax.min.js';
-// import './vendor/SplitText.min.js';
-
-
 window.onload = function() {
 
-    // var listItems = document.querySelectorAll('.m-list__item a');
-
-    // var index = 0;
-
-    // var mySplitText = new SplitText(listItems, {type:"lines"});
-    // var lines = mySplitText.lines;
-
-    // document.querySelectorAll('.m-list__item')[0].classList.add('show');
     var index = 0;
 
     var cta = document.querySelector('.m-list-cta');
