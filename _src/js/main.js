7f58f0714ec9a54d550b922fd7a66e0ed7e531d6
Luca De Blasio <deblaisioluca@gmail.com>
2017-06-08 00:31:03 +0200

    uodate routing

--- _src/js/main.js
+++ _src/js/main.js
@@ -2,7 +2,15 @@ window.onload = function() {
     var cta = document.querySelector('.m-list-cta'),
         items = document.querySelectorAll('.m-list__item'),
         links = document.querySelectorAll('.m-list a'),
+        hash = window.location.hash.split('#')[1],
+        index;
+
+    if(hash === undefined) {
         index = Math.round(Math.random()*items.length-1);
+        window.location.hash = items[index].getAttribute('data-slug');
+    } else {
+        index = hash;
+    }
 
     items[index].classList.add('show');
     
@@ -15,6 +23,13 @@ window.onload = function() {
             index++;    
         }
         
+        items[index].classList.add('show');
+        window.location.hash = items[index].getAttribute('data-slug');
+    });
+
+    window.addEventListener("hashchange", e => {
+        items[index].classList.remove('show');
+        index = window.location.hash.split('#')[1];
         items[index].classList.add('show');
     });
 
